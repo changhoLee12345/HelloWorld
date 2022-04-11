4591e1a628d1fbe88ffe9aa2b0fb7190fa1c662a
admin <admin@DESKTOP-14I4NOA>
2022-04-11 12:41:03 +0900

    first

---
+++ HelloWorld/src/co/edu/HelloWorld.java
@@ -0,0 +1,14 @@
+package co.edu;
+
+public class HelloWorld { // 클래스
+
+	public static void main(String[] args) { // 메소드
+		System.out.println("Hello, World!!");
+		System.out.println("안녕하세요, 이창호입니다.");
+		System.out.println("안녕히가세요..");
+	}
+
+	public static void sum() {
+
+	}
+}
