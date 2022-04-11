e01ec79706497a794abc101fc5450645157bda41
admin <admin@DESKTOP-14I4NOA>
2022-04-11 17:43:32 +0900

    마지막

---
+++ HelloWorld/src/co/edu/variable/OperatorExample.java
@@ -0,0 +1,24 @@
+package co.edu.variable;
+
+public class OperatorExample {
+	public static void main(String[] args) {
+		// 나머지: %
+		// 6/4 => 몫: 1, 나머지: 2
+		int result = 6 / 4;
+		int result2 = 6 % 4;
+		int val = 5;
+		if (val % 2 == 0) {
+			System.out.println("짝수입니다.");
+		} else {
+			System.out.println("홀수입니다.");
+		}
+		
+		// 75300 원.
+		// 
+		// 5만원 => ?개, 1만원 => ?개, 5천원 => ?개
+		// 1천원 => ?개, 5백원 => ?개, 1백원 => ?개
+		
+		// 75300원 5만원 1개, 1만원 2개, 5천원 1개, 1백원 3개.
+		
+	}
+}
