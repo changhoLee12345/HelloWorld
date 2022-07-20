a74b2ac04415b4512bd02e2b32b5da296e7fef56
winnie <winnie@DESKTOP-OKATUVK>
2022-07-20 09:03:40 +0900

    f

--- T20220719R/src/com/edu/StudentApp.java
+++ T20220719R/src/com/edu/StudentApp.java
@@ -10,6 +10,9 @@ public class StudentApp {
 	Scanner scn = new Scanner(System.in);
 
 	public void execute() {
-
+		System.out.println("1.등록 2.수정 3.리스트 4.종료");
+		System.out.print("선택> ");
+		
+		
 	}
 }
