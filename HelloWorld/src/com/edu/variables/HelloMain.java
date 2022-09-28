2f74bc8f8986db438ce4c99d2584460d67564f0e
ubuntuing <ubunt@email.com>
2022-09-29 08:45:24 +0900

    a

--- HelloWorld/src/com/edu/variables/HelloMain.java
+++ HelloWorld/src/com/edu/variables/HelloMain.java
@@ -1,23 +1,17 @@
 package com.edu.variables;
 
-import java.util.Arrays;
-
 public class HelloMain {
 	public static void main(String[] args) {
-		int num1 = 10, num2 = 20;
-		int result = sum(num1, num2);
+		String name = "hong";
+		String[] names = { "hong", "kang", "Choi" };
 
-		result = Math.subtractExact(num1, num2);
-
-		int[] intAry = {};
-		intAry = new int[] { 1, 2, 3, 4, 5 };
-		for (int i = 0; i < intAry.length; i++) {
-			System.out.println(intAry[i]);
+		for (String nm : names) {
+			if (nm == name) {
+				System.out.println("same");
+			} else {
+				System.out.println("diff");
+			}
 		}
-		
-		Arrays.asList(intAry).stream();
-
-		System.out.println(num1 + "과 " + num2 + "의 결과는 " + result + "입니다.");
 	}
 
 	public static int sum(int n1, int n2) {
