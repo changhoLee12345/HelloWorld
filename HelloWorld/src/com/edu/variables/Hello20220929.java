83e41bdbf9bf83879fff38be74db966391088e32
leadon <leadon@ubuntu>
2022-09-29 07:14:49 +0900

    a

---
+++ HelloWorld/src/com/edu/variables/Hello20220929.java
@@ -0,0 +1,9 @@
+package com.edu.variables;
+
+public class Hello20220929 {
+	public static void main(String[] args) {
+		int[] intAry = { 23, 22, 21, 20 };
+		HelloMain.sortAry(intAry);
+		HelloMain.printAry(intAry);
+	}
+}
--- HelloWorld/src/com/edu/variables/HelloMain.java
+++ HelloWorld/src/com/edu/variables/HelloMain.java
@@ -1,13 +1,12 @@
 package com.edu.variables;
 
-import java.util.Arrays;
-
 public class HelloMain {
 
 	public static void main(String[] args) {
-		int[] intAry = { 34, 21, 18 };
+		int[] intAry = { 34, 21, 18, 12 };
 
 		// sorting.
+		// 1)
 		int temp = 0;
 		if (intAry[0] > intAry[1]) {
 			temp = intAry[0];
@@ -19,16 +18,57 @@ public class HelloMain {
 			intAry[1] = intAry[2];
 			intAry[2] = temp;
 		}
+		if (intAry[2] > intAry[3]) {
+			temp = intAry[2];
+			intAry[2] = intAry[3];
+			intAry[3] = temp;
+		}
+		printAry(intAry);
+		// 2)
 		if (intAry[0] > intAry[1]) {
 			temp = intAry[0];
 			intAry[0] = intAry[1];
 			intAry[1] = temp;
 		}
+		if (intAry[1] > intAry[2]) {
+			temp = intAry[1];
+			intAry[1] = intAry[2];
+			intAry[2] = temp;
+		}
+		printAry(intAry);
+		// 3)
+		if (intAry[0] > intAry[1]) {
+			temp = intAry[0];
+			intAry[0] = intAry[1];
+			intAry[1] = temp;
+		}
+		printAry(intAry);
 
 		// sorted.
-		for (int num : intAry) {
-			System.out.println(num);
+//		for (int num : intAry) {
+//			System.out.println(num);
+//		}
+	}
+
+	public static void sortAry(int[] ary) {
+		int temp = 0;
+		for (int j = 0; j < ary.length - 1; j++) {
+			for (int i = 0; i < ary.length - (j + 1); i++) {
+				System.out.println("j=> " + j + ", i=> " + i);
+				if (ary[i] > ary[i + 1]) {
+					temp = ary[i];
+					ary[i] = ary[i + 1];
+					ary[i + 1] = temp;
+				}
+			}
 		}
+	}
+
+	public static void printAry(int[] ary) {
+		for (int i = 0; i < ary.length; i++)
+			System.out.printf("%d ", ary[i]);
+
+		System.out.println();
 	}
 
 }
