b68d90cc731357f0406ff9d72708fc3617bc4db4
winnie <winnie@email>
2022-10-07 23:53:16 +0900

    d

---
+++ GuiProject/src/gui/Model.java
@@ -0,0 +1,38 @@
+package gui;
+
+public class Model {
+	String name;
+	String birth;
+	String tel;
+
+	public Model(String name, String birth, String tel) {
+		this.birth = birth;
+		this.name = name;
+		this.tel = tel;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+}
