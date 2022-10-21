d8a2605d3c6409f82c464ff9b48ad8fa81b861f1
changhoLee12345 <53467973+changhoLee12345@users.noreply.github.com>
2022-10-22 03:04:48 +0900

    Delete P20220401 directory

--- P20220401/src/com/test/CollegeStudent.java
+++
@@ -1,7 +0,0 @@
-package com.test;
-
-public class CollegeStudent {
-	private String profName; // 담당교수.
-	private String major; // 전공정보.
-
-}
--- P20220401/src/com/test/HighStudent.java
+++
@@ -1,7 +0,0 @@
-package com.test;
-
-public class HighStudent {
-	private String teacherName; // 담임선생님.
-	private String groupName; // 반이름.
-
-}
