5e129e7efd15193b62e9fe83983942feff7aac27
your <you@email.co>
2023-06-05 13:09:14 +0900

    ㄹ

--- 20210813/src/co/yedam/chap6/insa/Departments.java
+++ 20210813/src/co/yedam/chap6/insa/Departments.java
@@ -1,25 +1,28 @@
 package co.yedam.chap6.insa;
 
 public class Departments {
-	int departmentId; //부서번호
-	String departmentName;//부서명
-	int managerId;//메니저
-	int locationId;//지역
-	
-	//생성자 오버로딩
-	//기본생성자
-	Departments(){}
-	
-	//부서명 초기화 
-	Departments(String departmentName){
+	int departmentId; // 부서번호
+	String departmentName;// 부서명
+	int managerId;// 메니저
+	int locationId;// 지역
+
+	// 생성자 오버로딩
+	// 기본생성자
+	Departments() {
+	}
+
+	// 부서명 초기화
+	Departments(String departmentName) {
 		this.departmentName = departmentName;
 	}
-	//부서번호, 부서명 초기화
-	Departments(int departmentId, String departmentName){
+
+	// 부서번호, 부서명 초기화
+	Departments(int departmentId, String departmentName) {
 		this.departmentId = departmentId;
 		this.departmentName = departmentName;
 	}
-	//필드 4개 초기화
+
+	// 필드 4개 초기화
 	public Departments(int departmentId, String departmentName, int managerId, int locationId) {
 		this.departmentId = departmentId;
 		this.departmentName = departmentName;
@@ -32,6 +35,5 @@ public class Departments {
 		return "Departments [departmentId=" + departmentId + ", departmentName=" + departmentName + ", managerId="
 				+ managerId + ", locationId=" + locationId + "]";
 	}
-	
 
 }
--- 20210813/src/co/yedam/chap6/insa/MainApp.java
+++ 20210813/src/co/yedam/chap6/insa/MainApp.java
@@ -6,24 +6,24 @@ public class MainApp {
 		Departments dept = new Departments();
 		dept.departmentId = 10;
 		dept.departmentName = "인사";
-		
+
 		Departments dept1 = new Departments("기획");
-		Departments dept2 = new Departments(30,"개발");
-		Departments dept3 = new Departments(40,"총무", 100, 7000);
-		
+		Departments dept2 = new Departments(30, "개발");
+		Departments dept3 = new Departments(40, "총무", 100, 7000);
+
 		System.out.println(dept1.toString());
 		System.out.println(dept2);
-		
-		//사원 
-		Employees emp = new Employees(100,"김유미");
-		Employees emp1 = new Employees(101,"홍길동", dept1);
-		Employees emp2 = new Employees(102,"김기자", new Departments(30,"개발"));
-		
-		//emp1 사원의 부서명을 출력
-		System.out.println( emp1.dept.departmentName );
-		
-		Employees emp3 = new Employees(100,"김유미");
-		System.out.println(emp3.dept);   //null
+
+		// 사원
+		Employees emp = new Employees(100, "김유미");
+		Employees emp1 = new Employees(101, "홍길동", dept1);
+		Employees emp2 = new Employees(102, "김기자", new Departments(30, "개발"));
+
+		// emp1 사원의 부서명을 출력
+		System.out.println(emp1.dept.departmentName);
+
+		Employees emp3 = new Employees(100, "김유미");
+		System.out.println(emp3.dept); // null
 		System.out.println(emp3.dept.departmentName);
 	}
 
