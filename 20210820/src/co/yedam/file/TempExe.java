a31c30b0f5b57d86ce8916bf0d2b2e6345d17bf2
note <note@example.com>
2023-07-24 17:55:59 +0900

    temp

---
+++ 20210820/src/co/yedam/file/TempExe.java
@@ -0,0 +1,11 @@
+package co.yedam.file;
+
+import java.util.Date;
+
+public class TempExe {
+	public static void main(String[] args) {
+		Date today = new Date();
+		System.out.println("start");
+		System.out.println(today.getTime());
+	}
+}
