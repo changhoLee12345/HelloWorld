0991fb16eca5ac4e3a0e3c1609500b7e8cfff894
you <you@email.com>
2024-03-27 18:20:13 +0900

    mybatis...

--- mavenProject/src/main/java/co/yedam/mavenProject/App.java
+++ mavenProject/src/main/java/co/yedam/mavenProject/App.java
@@ -1,6 +1,15 @@
 package co.yedam.mavenProject;
 
-import co.yedam.mavenProject.jdbc.ManageExe;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import co.yedam.mavenProject.vo.MemberVO;
 
 /**
  * Hello world!
@@ -9,6 +18,33 @@ import co.yedam.mavenProject.jdbc.ManageExe;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		ManageExe.exe();
+		try {
+			Reader reader = Resources.getResourceAsReader("config/mybatis-config.xml");
+			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(reader);
+			reader.close();
+
+			SqlSession session = factory.openSession(true);
+
+			MemberVO ivo = new MemberVO("user06", "0000", "사용자5", "010-0000-9999");
+//			session.insert("co.yedam.mavenProject.mapper.QueryMapper.insertMember", ivo);
+			session.delete("co.yedam.mavenProject.mapper.QueryMapper.deleteMember", ivo.getMemId());
+
+//			MemberVO mvo = session.selectOne("co.yedam.mavenProject.mapper.QueryMapper.selectMember", "user01");
+//			System.out.println(mvo.toString());
+
+			List<MemberVO> list = session.selectList("co.yedam.mavenProject.mapper.QueryMapper.memberList");
+
+			if (list.size() > 0) {
+				list.forEach(System.out::println);
+
+			} else {
+				System.out.println("no data.");
+
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- mavenProject/src/main/java/co/yedam/mavenProject/vo/MemberVO.java
+++ mavenProject/src/main/java/co/yedam/mavenProject/vo/MemberVO.java
@@ -3,12 +3,21 @@ package co.yedam.mavenProject.vo;
 import java.util.Date;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode.Exclude;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 public class MemberVO {
+	@NonNull
 	private String memId;
+	@NonNull
 	private String memPw;
+	@NonNull
 	private String memName;
+	@NonNull
 	private String memPhone;
+
 	private Date creationDate;
 }
